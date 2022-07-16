a84ceadc9418d1a0e11791667de84a0446e3d94a
Virgy <email@gmail.com>
2022-07-16 19:33:18 +0500

    mod

--- js/script_rev.js
+++ js/script_rev.js
@@ -3,8 +3,8 @@
 // "use strict";
 
 // alert('Hello');
-// const result = confirm("Are you here?");
-// console.log(result);
+const result = confirm("Are you here?");
+console.log(result);
 
 // const answer = +prompt('Are you 18?', '18');
 // console.log(answer+5);
