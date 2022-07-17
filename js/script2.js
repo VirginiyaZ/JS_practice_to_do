f23b9da855eb9436dc2a9f0c102a7ce6bd4203ae
Virgy <email@gmail.com>
2022-07-17 18:11:32 +0500

    practical script2 updated

--- js/script2.js
+++ js/script2.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,12 +10,30 @@ const personalMovieDB = {
     privat: false
 
 };
-const ans =  prompt('Один из последних просмотренных фильмов?', ''), 
-      ans2 = prompt("На сколько оцените его?", ''),
-      ans3 = prompt('Один из последних просмотренных фильмов?', ''), 
-      ans4 = prompt("На сколько оцените его?", '');
 
-personalMovieDB.movies[ans] = ans2;
-personalMovieDB.movies[ans3] = ans4;
+
+for (let i = 0; i < 2; i++){
+    const ans =  prompt('Один из последних просмотренных фильмов?', ''), 
+          ans2 = prompt("На сколько оцените его?", '');
+
+    if (ans != null && ans2 != null && ans != '' && ans2 != '' && ans.length < 50){
+        personalMovieDB.movies[ans] = ans2;
+        console.log('done');
+    } else {
+        console.log('Error');
+        i--;
+    }
+  
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('You watched a few movies');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log('You are awesome movie -watcher');
+} else if (personalMovieDB.count >= 30) {
+    console.log('You are Cinemaddict');
+} else {
+    console.log('Error');
+}
 
 console.log(personalMovieDB);
