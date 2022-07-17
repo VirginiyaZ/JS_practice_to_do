f976e6f0663b07235feaba7b5b9ddbba31f6e523
Virgy <email@gmail.com>
2022-07-17 17:20:11 +0500

    tasks answers been added

--- js/while_for.js
+++ js/while_for.js
@@ -34,18 +34,18 @@ for (let i = 0; i < 3; i++){
 
 }
 //  => x-mass tree 
-let result = "";
-const leng = 7;
+// let result = "";
+// const leng = 7;
 
-for (let i = 1; i < leng; i++){
-    for (let j = 0; j < i; j++){
-        result += '*';
-    }
+// for (let i = 1; i < leng; i++){
+//     for (let j = 0; j < i; j++){
+//         result += '*';
+//     }
 
-    result += '\n';
-}
+//     result += '\n';
+// }
 
-console.log(result);
+// console.log(result);
 
 
 // first: for (let i = 0; i < 3; i++){
@@ -88,3 +88,51 @@ for (let i = 5; i < 11; i++){
     arrOfNumbers[i-5] = i;
     console.log(arrOfNumbers);
 }
+
+//       *
+//      ***
+//     *****
+//    *******
+//   *********
+//  ***********
+let result = "";
+const leng = 5;
+
+for (let i = 0; i <= leng; i++){
+    for(let j= 0; j <leng -i; j++){
+        result += " ";
+    }
+    for (let j = 0; j < 2*i +1; j++){
+    result +='*';
+}
+result += "\n";
+}
+
+console.log(result);
+
+    
+const arr = [3, 5, 8, 16, 20, 23, 50];
+const res = [];
+
+for (let i =0; i < arr.length; i++){
+    res[i] = arr[i];
+}
+console.log(res);
+
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// for (let i = 0; i < data.length; i++) {
+//     if (typeof(data[i]) === 'number') {
+//         data[i] = data[i] * 2;
+//     } else if (typeof(data[i]) === 'string') {
+//         data[i] = `${data[i]} - done`;
+//     }
+// }
+// console.log(data);
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+const resul = [];
+for (let i =1; i <= data.length; i++){
+    resul[i-1] = data[data.length - i];
+    }
+console.log(resul);
